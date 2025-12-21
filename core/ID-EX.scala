d27e3683b94a6f381a27712de678f81c3cd1935f
dashygo097 <dashygo097@github.com>
2025-12-21 16:30:26 +0800

    optim: decoder

--- core/ID-EX.scala
+++ core/ID-EX.scala
@@ -16,6 +16,18 @@ class ID_EX extends Module {
   val ID_MEM_READ   = IO(Input(Bool()))
   val ID_MEM_WRITE  = IO(Input(Bool()))
 
+  val ID_IS_ALU     = IO(Input(Bool()))
+  val ID_IS_ALU_IMM = IO(Input(Bool()))
+  val ID_IS_LOAD    = IO(Input(Bool()))
+  val ID_IS_STORE   = IO(Input(Bool()))
+  val ID_IS_BRANCH  = IO(Input(Bool()))
+  val ID_IS_JAL     = IO(Input(Bool()))
+  val ID_IS_JALR    = IO(Input(Bool()))
+  val ID_IS_LUI     = IO(Input(Bool()))
+  val ID_IS_AUIPC   = IO(Input(Bool()))
+  val ID_IS_SYSTEM  = IO(Input(Bool()))
+  val ID_IS_FENCE   = IO(Input(Bool()))
+
   // Data
   val ID_PC       = IO(Input(UInt(32.W)))
   val ID_INST     = IO(Input(UInt(32.W)))
@@ -36,16 +48,29 @@ class ID_EX extends Module {
   val EX_REG_WRITE  = IO(Output(Bool()))
   val EX_MEM_READ   = IO(Output(Bool()))
   val EX_MEM_WRITE  = IO(Output(Bool()))
-  val EX_PC         = IO(Output(UInt(32.W)))
-  val EX_INST       = IO(Output(UInt(32.W)))
-  val EX_RS1_DATA   = IO(Output(UInt(32.W)))
-  val EX_RS2_DATA   = IO(Output(UInt(32.W)))
-  val EX_IMM        = IO(Output(UInt(32.W)))
-  val EX_RD         = IO(Output(UInt(5.W)))
-  val EX_RS1        = IO(Output(UInt(5.W)))
-  val EX_RS2        = IO(Output(UInt(5.W)))
-  val EX_FUNCT3     = IO(Output(UInt(3.W)))
-  val EX_OPCODE     = IO(Output(UInt(7.W)))
+
+  val EX_IS_ALU     = IO(Output(Bool()))
+  val EX_IS_ALU_IMM = IO(Output(Bool()))
+  val EX_IS_LOAD    = IO(Output(Bool()))
+  val EX_IS_STORE   = IO(Output(Bool()))
+  val EX_IS_BRANCH  = IO(Output(Bool()))
+  val EX_IS_JAL     = IO(Output(Bool()))
+  val EX_IS_JALR    = IO(Output(Bool()))
+  val EX_IS_LUI     = IO(Output(Bool()))
+  val EX_IS_AUIPC   = IO(Output(Bool()))
+  val EX_IS_SYSTEM  = IO(Output(Bool()))
+  val EX_IS_FENCE   = IO(Output(Bool()))
+
+  val EX_PC       = IO(Output(UInt(32.W)))
+  val EX_INST     = IO(Output(UInt(32.W)))
+  val EX_RS1_DATA = IO(Output(UInt(32.W)))
+  val EX_RS2_DATA = IO(Output(UInt(32.W)))
+  val EX_IMM      = IO(Output(UInt(32.W)))
+  val EX_RD       = IO(Output(UInt(5.W)))
+  val EX_RS1      = IO(Output(UInt(5.W)))
+  val EX_RS2      = IO(Output(UInt(5.W)))
+  val EX_FUNCT3   = IO(Output(UInt(3.W)))
+  val EX_OPCODE   = IO(Output(UInt(7.W)))
 
   // Registers
   val alu_op_r_reg   = RegInit(0.U(3.W))
@@ -55,16 +80,29 @@ class ID_EX extends Module {
   val reg_write_reg  = RegInit(false.B)
   val mem_read_reg   = RegInit(false.B)
   val mem_write_reg  = RegInit(false.B)
-  val pc_reg         = RegInit(0.U(32.W))
-  val inst_reg       = RegInit(0.U(32.W))
-  val rs1_data_reg   = RegInit(0.U(32.W))
-  val rs2_data_reg   = RegInit(0.U(32.W))
-  val imm_reg        = RegInit(0.U(32.W))
-  val rd_reg         = RegInit(0.U(5.W))
-  val rs1_reg        = RegInit(0.U(5.W))
-  val rs2_reg        = RegInit(0.U(5.W))
-  val funct3_reg     = RegInit(0.U(3.W))
-  val opcode_reg     = RegInit(0.U(7.W))
+
+  val is_alu_reg     = RegInit(false.B)
+  val is_alu_imm_reg = RegInit(false.B)
+  val is_load_reg    = RegInit(false.B)
+  val is_store_reg   = RegInit(false.B)
+  val is_branch_reg  = RegInit(false.B)
+  val is_jal_reg     = RegInit(false.B)
+  val is_jalr_reg    = RegInit(false.B)
+  val is_lui_reg     = RegInit(false.B)
+  val is_auipc_reg   = RegInit(false.B)
+  val is_system_reg  = RegInit(false.B)
+  val is_fence_reg   = RegInit(false.B)
+
+  val pc_reg       = RegInit(0.U(32.W))
+  val inst_reg     = RegInit(0.U(32.W))
+  val rs1_data_reg = RegInit(0.U(32.W))
+  val rs2_data_reg = RegInit(0.U(32.W))
+  val imm_reg      = RegInit(0.U(32.W))
+  val rd_reg       = RegInit(0.U(5.W))
+  val rs1_reg      = RegInit(0.U(5.W))
+  val rs2_reg      = RegInit(0.U(5.W))
+  val funct3_reg   = RegInit(0.U(3.W))
+  val opcode_reg   = RegInit(0.U(7.W))
 
   when(FLUSH) {
     alu_op_r_reg   := 0.U
@@ -74,16 +112,30 @@ class ID_EX extends Module {
     reg_write_reg  := false.B
     mem_read_reg   := false.B
     mem_write_reg  := false.B
-    pc_reg         := 0.U
-    inst_reg       := 0.U
-    rs1_data_reg   := 0.U
-    rs2_data_reg   := 0.U
-    imm_reg        := 0.U
-    rd_reg         := 0.U
-    rs1_reg        := 0.U
-    rs2_reg        := 0.U
-    funct3_reg     := 0.U
-    opcode_reg     := 0.U
+
+    is_alu_reg     := false.B
+    is_alu_imm_reg := false.B
+    is_load_reg    := false.B
+    is_store_reg   := false.B
+    is_branch_reg  := false.B
+    is_jal_reg     := false.B
+    is_jalr_reg    := false.B
+    is_lui_reg     := false.B
+    is_auipc_reg   := false.B
+    is_system_reg  := false.B
+    is_fence_reg   := false.B
+
+    pc_reg       := 0.U
+    inst_reg     := 0.U
+    rs1_data_reg := 0.U
+    rs2_data_reg := 0.U
+    imm_reg      := 0.U
+    rd_reg       := 0.U
+    rs1_reg      := 0.U
+    rs2_reg      := 0.U
+    funct3_reg   := 0.U
+    opcode_reg   := 0.U
+
   }.elsewhen(!STALL) {
     alu_op_r_reg   := ID_ALU_OP_R
     alu_is_sub_reg := ID_ALU_IS_SUB
@@ -92,16 +144,29 @@ class ID_EX extends Module {
     reg_write_reg  := ID_REG_WRITE
     mem_read_reg   := ID_MEM_READ
     mem_write_reg  := ID_MEM_WRITE
-    pc_reg         := ID_PC
-    inst_reg       := ID_INST
-    rs1_data_reg   := ID_RS1_DATA
-    rs2_data_reg   := ID_RS2_DATA
-    imm_reg        := ID_IMM
-    rd_reg         := ID_RD
-    rs1_reg        := ID_RS1
-    rs2_reg        := ID_RS2
-    funct3_reg     := ID_FUNCT3
-    opcode_reg     := ID_OPCODE
+
+    is_alu_reg     := ID_IS_ALU
+    is_alu_imm_reg := ID_IS_ALU_IMM
+    is_load_reg    := ID_IS_LOAD
+    is_store_reg   := ID_IS_STORE
+    is_branch_reg  := ID_IS_BRANCH
+    is_jal_reg     := ID_IS_JAL
+    is_jalr_reg    := ID_IS_JALR
+    is_lui_reg     := ID_IS_LUI
+    is_auipc_reg   := ID_IS_AUIPC
+    is_system_reg  := ID_IS_SYSTEM
+    is_fence_reg   := ID_IS_FENCE
+
+    pc_reg       := ID_PC
+    inst_reg     := ID_INST
+    rs1_data_reg := ID_RS1_DATA
+    rs2_data_reg := ID_RS2_DATA
+    imm_reg      := ID_IMM
+    rd_reg       := ID_RD
+    rs1_reg      := ID_RS1
+    rs2_reg      := ID_RS2
+    funct3_reg   := ID_FUNCT3
+    opcode_reg   := ID_OPCODE
   }
 
   EX_ALU_OP_R   := alu_op_r_reg
@@ -111,14 +176,27 @@ class ID_EX extends Module {
   EX_REG_WRITE  := reg_write_reg
   EX_MEM_READ   := mem_read_reg
   EX_MEM_WRITE  := mem_write_reg
-  EX_PC         := pc_reg
-  EX_INST       := inst_reg
-  EX_RS1_DATA   := rs1_data_reg
-  EX_RS2_DATA   := rs2_data_reg
-  EX_IMM        := imm_reg
-  EX_RD         := rd_reg
-  EX_RS1        := rs1_reg
-  EX_RS2        := rs2_reg
-  EX_FUNCT3     := funct3_reg
-  EX_OPCODE     := opcode_reg
+
+  EX_IS_ALU     := is_alu_reg
+  EX_IS_ALU_IMM := is_alu_imm_reg
+  EX_IS_LOAD    := is_load_reg
+  EX_IS_STORE   := is_store_reg
+  EX_IS_BRANCH  := is_branch_reg
+  EX_IS_JAL     := is_jal_reg
+  EX_IS_JALR    := is_jalr_reg
+  EX_IS_LUI     := is_lui_reg
+  EX_IS_AUIPC   := is_auipc_reg
+  EX_IS_SYSTEM  := is_system_reg
+  EX_IS_FENCE   := is_fence_reg
+
+  EX_PC       := pc_reg
+  EX_INST     := inst_reg
+  EX_RS1_DATA := rs1_data_reg
+  EX_RS2_DATA := rs2_data_reg
+  EX_IMM      := imm_reg
+  EX_RD       := rd_reg
+  EX_RS1      := rs1_reg
+  EX_RS2      := rs2_reg
+  EX_FUNCT3   := funct3_reg
+  EX_OPCODE   := opcode_reg
 }
