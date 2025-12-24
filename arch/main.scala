8b66386f035413d23ca60db89d9470fa1f9ad01c
dashygo097 <dashygo097@github.com>
2025-12-24 21:51:05 +0800

    feat: arch lib for coreutils

---
+++ arch/main.scala
@@ -0,0 +1,10 @@
+package arch
+
+import configs._
+
+object ISA  extends Field[String]("rv32i")
+object XLen extends Field[Int](32)
+
+object RV32I_CPU_App extends App {
+  println(s"Building a CPU with ISA = ${ISA()} and XLen = ${XLen()}")
+}
