07c636b31c1b9074df8b3caa490dd1b0d4c82c8a
dashygo097 <dashygo097@gmail.com>
2025-12-31 15:20:06 +0800

    [arch]: bru works for now

--- arch/core/bru/abstract.scala
+++ arch/core/bru/abstract.scala
@@ -4,5 +4,6 @@ import chisel3._
 
 trait BruUtilities {
   def branchTypeWidth: Int
+  def isJump(fnType: UInt): Bool
   def fn(src1: UInt, src2: UInt, fnType: UInt): Bool
 }
--- arch/core/bru/bru.scala
+++ arch/core/bru/bru.scala
@@ -3,20 +3,20 @@ package arch.core.bru
 import arch.configs._
 import chisel3._
 
-// TODO: JAL/JALR handling
 class Bru(implicit p: Parameters) extends Module {
   override def desiredName: String = s"${p(ISA)}_bru"
 
   val utils = BruUtilitiesFactory.getOrThrow(p(ISA))
 
   val en     = IO(Input(Bool()))
+  val pc     = IO(Input(UInt(p(XLen).W)))
   val src1   = IO(Input(UInt(p(XLen).W)))
   val src2   = IO(Input(UInt(p(XLen).W)))
   val imm    = IO(Input(UInt(p(XLen).W)))
   val brType = IO(Input(UInt(utils.branchTypeWidth.W)))
+  val taken  = IO(Output(Bool()))
   val target = IO(Output(UInt(p(XLen).W)))
-  val cmp    = IO(Output(Bool()))
 
-  cmp    := en && utils.fn(src1, src2, brType)
-  target := src1 + imm
+  taken  := en && utils.fn(src1, src2, brType)
+  target := pc + imm
 }
