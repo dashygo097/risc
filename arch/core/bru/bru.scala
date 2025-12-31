fa85cc4da1ae91bfdec63d068c886c8c6521f8a1
dashygo097 <dashygo097@github.com>
2025-12-31 14:34:30 +0800

    [arch]: bru checkpoint

--- arch/core/bru/bru.scala
+++ arch/core/bru/bru.scala
@@ -3,6 +3,7 @@ package arch.core.bru
 import arch.configs._
 import chisel3._
 
+// TODO: JAL/JALR handling
 class Bru(implicit p: Parameters) extends Module {
   override def desiredName: String = s"${p(ISA)}_bru"
 
@@ -11,8 +12,11 @@ class Bru(implicit p: Parameters) extends Module {
   val en     = IO(Input(Bool()))
   val src1   = IO(Input(UInt(p(XLen).W)))
   val src2   = IO(Input(UInt(p(XLen).W)))
+  val imm    = IO(Input(UInt(p(XLen).W)))
   val brType = IO(Input(UInt(utils.branchTypeWidth.W)))
+  val target = IO(Output(UInt(p(XLen).W)))
   val cmp    = IO(Output(Bool()))
 
-  cmp := en && utils.fn(src1, src2, brType)
+  cmp    := en && utils.fn(src1, src2, brType)
+  target := src1 + imm
 }
