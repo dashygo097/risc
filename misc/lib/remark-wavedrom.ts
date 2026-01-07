92987561eb5f6b7896935afae137aa7d6f8f6bfb
dashygo097 <dashygo097@github.com>
2026-01-07 23:42:51 +0800

    [arch]: [DOCS] wavedrom OKKKKKK

--- misc/lib/remark-wavedrom.ts
+++ misc/lib/remark-wavedrom.ts
@@ -1,25 +1,28 @@
 import { visit } from "unist-util-visit";
-import type { Root } from "mdast";
+import type { Root, Code } from "mdast";
+import type { Parent } from "unist";
 
 export function remarkWaveDrom() {
   return (tree: Root) => {
-    visit(tree, "code", (node: any, index: number, parent: any) => {
-      if (node.lang === "wavedrom") {
-        const value = node.value;
-
-        parent.children[index] = {
-          type: "mdxJsxFlowElement",
-          name: "WaveDrom",
-          attributes: [],
-          children: [
-            {
-              type: "text",
-              value: value,
-            },
-          ],
-        };
-      }
-    });
+    visit(
+      tree,
+      "code",
+      (node: Code, index: number | undefined, parent: Parent | undefined) => {
+        if (node.lang === "wavedrom" && parent && typeof index === "number") {
+          parent.children[index] = {
+            type: "mdxJsxFlowElement",
+            name: "WaveDrom",
+            attributes: [],
+            children: [
+              {
+                type: "text",
+                value: node.value,
+              },
+            ],
+          } as unknown as Code;
+        }
+      },
+    );
   };
 }
 
