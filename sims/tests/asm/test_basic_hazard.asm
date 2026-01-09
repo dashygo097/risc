9bde78c7def92b9c5df228fb60c933339fd17c7d
dashygo097 <dashygo097@github.com>
2026-01-09 16:31:56 +0800

    [DEMU]: unified test standards for basic tests; [ISSUE]: branch-load-hazard detected for the current core design

--- sims/tests/asm/test_basic_hazard.asm
+++ sims/tests/asm/test_basic_hazard.asm
@@ -4,6 +4,9 @@
 _start:
     lui x10, 0x80000
 
+    addi x7, x0, 0  # initialize x7 to 0
+    addi x11, x0, 0 #  initialize x11 to 0
+
     # Store-Load Hazard
     addi x1, x0, 10
     sw x1, 0(x10)
@@ -17,18 +20,18 @@ _start:
     lw x5, 0(x10)    # Hazard: load followed by branch
     addi x6, x0, 10
     beq x5, x6, branch_taken 
-    addi x7, x0, -2 # failure
+    addi x7, x7, -2 # failure
 
 branch_taken:
-    addi x7, x0, 1 # success
+    addi x7, x7, 1 # success
 
     # Load-Branch Hazard (Not Taken)
     lw x8, 0(x10)    # Hazard: load followed by branch
     addi x9, x0, 20
     beq x8, x9, branch_not_taken
-    addi x7, x0, 1 # success 
+    addi x11, x11, 1 # success 
 
 branch_not_taken:
-    addi x7, x0, -2 # failure
+    addi x11, x11, -2 # failure
 
     ebreak
--- sims/tests/asm/test_basic_jump.asm
+++ sims/tests/asm/test_basic_jump.asm
@@ -4,19 +4,22 @@
 _start:
     lui x10, 0x80000
 
+    addi x2, x0, 0      # Initialize x2 to 0
+    addi x5, x0, 0      # Initialize x5 to 0
+
     # JAL 
     jal x1, label1      # Jump to label1, save PC+4 in x1
-    addi x2, x0, -2     # Should NOT execute
+    addi x2, x2, -2     # Should NOT execute
     
 label1:
-    addi x2, x0, 1      # x3 = 1 (success)
+    addi x2, x2, 1      # x2 = 1 (success)
 
     # JALR
-    la x3, label2     # Load address of label2 into x4
-    jalr x4, 0(x3)   # Jump to address in x4, do not save return address
-    addi x5, x0, -2    # Should NOT execute
+    la x3, label2     # Load address of label2 into x3
+    jalr x4, 0(x3)   # Jump to address in x3, do not save return address
+    addi x5, x5, -2    # Should NOT execute
 
 label2:
-    addi x5, x0, 1     # x7 = 1 (success)
+    addi x5, x5, 1     # x5 = 1 (success)
     
     ebreak
