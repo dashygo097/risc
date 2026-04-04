1a94656156b465513471c61f65c90d6d83509120
dashygo097 <dashygo097@gmail.com>
2026-04-04 17:51:31 +0800

    [risc] placeholder for load_unit and store_buffer

---
+++ arch/core/lsu/LoadUnit.scala
@@ -0,0 +1,8 @@
+package arch.core.lsu
+
+import arch.configs._
+import chisel3._
+
+class LoadUnit(implicit p: Parameters) extends Module {
+  override def desiredName: String = s"${p(ISA)}_load_unit"
+}
