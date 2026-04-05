5c3d3ce189a6a9036c9519c346b24f8e9d3a07df
dashygo097 <dashygo097@gmail.com>
2026-04-05 18:36:57 +0800

    [risc] update framework: new protobuf types; ooo scheduler architecture for different excution models

--- arch/core/ooo/FunctionalUnit.scala
+++ arch/core/ooo/FunctionalUnit.scala
@@ -5,17 +5,13 @@ import chisel3._
 import chisel3.util._
 
 class FunctionalUnitResp(implicit p: Parameters) extends Bundle {
-  val valid  = Bool()
-  val result = UInt(p(XLen).W)
-  val rd     = UInt(log2Ceil(p(NumArchRegs)).W)
-
+  val result  = UInt(p(XLen).W)
+  val rd      = UInt(log2Ceil(p(NumArchRegs)).W)
   val rob_tag = UInt(log2Ceil(p(ROBSize)).W)
 }
 
 abstract class FunctionalUnit(implicit p: Parameters) extends Module {
-  def busy: Bool = !req.ready
-
   val req   = IO(Flipped(Decoupled(new MicroOp)))
-  val resp  = IO(Decoupled(new FunctionalUnitResp))
+  val resp  = IO(Valid(new FunctionalUnitResp))
   val flush = IO(Input(Bool()))
 }
