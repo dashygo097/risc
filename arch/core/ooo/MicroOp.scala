1456e97048c848d76bf00a7f17e067f19504c969
dashygo097 <dashygo097@gmail.com>
2026-04-08 23:31:44 +0800

    [risc] extensibility(?) for functional units

--- arch/core/ooo/MicroOp.scala
+++ arch/core/ooo/MicroOp.scala
@@ -5,10 +5,10 @@ import chisel3._
 import chisel3.util.log2Ceil
 
 class MicroOp(implicit p: Parameters) extends Bundle {
-  val pc       = UInt(p(XLen).W)
-  val instr    = UInt(p(ILen).W)
+  val pc    = UInt(p(XLen).W)
+  val instr = UInt(p(ILen).W)
 
-  val fu_id    = UInt(log2Ceil(p(FunctionalUnits).size).W)
+  val fu_id = UInt(log2Ceil(p(FunctionalUnits).size).W)
 
   val rs1      = UInt(log2Ceil(p(NumArchRegs)).W)
   val rs2      = UInt(log2Ceil(p(NumArchRegs)).W)
@@ -16,5 +16,5 @@ class MicroOp(implicit p: Parameters) extends Bundle {
   val rs1_data = UInt(p(XLen).W)
   val rs2_data = UInt(p(XLen).W)
 
-  val rob_tag  = UInt(log2Ceil(p(ROBSize)).W)
+  val rob_tag = UInt(log2Ceil(p(ROBSize)).W)
 }
