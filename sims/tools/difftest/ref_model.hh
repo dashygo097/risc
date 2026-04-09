c73ee2ed21dc370cdc753e1bbe2412dc9f339fc6
dashygo097 <dashygo097@github.com>
2026-04-09 16:45:16 +0800

    [demu] integrated isa target for difftest

--- sims/tools/difftest/ref_model.hh
+++ sims/tools/difftest/ref_model.hh
@@ -1,14 +1,16 @@
 #pragma once
 
+#include "demu/isa/isa.hh"
 #include <cstdint>
 #include <memory>
 #include <string>
 
 namespace demu::difftest {
+using namespace isa;
 
-struct riscv32_CPU_state {
-  uint32_t gpr[32];
-  uint32_t pc;
+struct CPU_state {
+  word_t gpr[NUM_GPRS];
+  addr_t pc;
 };
 
 class IRefModel {
@@ -16,16 +18,16 @@ public:
   virtual ~IRefModel() = default;
 
   virtual auto init() -> bool = 0;
-  virtual void sync_memory(uint32_t addr, size_t size, const void *data) = 0;
+  virtual void sync_memory(addr_t addr, size_t size, const void *data) = 0;
   virtual void step(uint64_t n) = 0;
 
   virtual void push_state() = 0;
   virtual void pull_state() = 0;
 
-  [[nodiscard]] virtual auto get_pc() const -> uint32_t = 0;
-  [[nodiscard]] virtual auto get_reg(uint8_t idx) const -> uint32_t = 0;
-  virtual void set_pc(uint32_t pc) = 0;
-  virtual void set_reg(uint8_t idx, uint32_t val) = 0;
+  [[nodiscard]] virtual auto get_pc() const -> addr_t = 0;
+  [[nodiscard]] virtual auto get_reg(uint8_t idx) const -> word_t = 0;
+  virtual void set_pc(addr_t pc) = 0;
+  virtual void set_reg(uint8_t idx, word_t val) = 0;
 };
 
 auto create_ref_model(const std::string &so_path) -> std::unique_ptr<IRefModel>;
