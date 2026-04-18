d4e45b4a30205fa0abc5c8da0969b85a33a4513d
dashygo097 <dashygo097@gmail.com>
2026-04-18 23:43:35 +0800

    [risc] remove warns

--- arch/core/lsu/LSUFu.scala
+++ arch/core/lsu/LSUFu.scala
@@ -6,7 +6,10 @@ import arch.core.pma._
 import arch.configs._
 import vopts.mem.cache.CacheIO
 import chisel3._
-import chisel3.util._
+
+object LsuFuState extends ChiselEnum {
+  val IDLE, WAIT_RESP, DONE = Value
+}
 
 class LsuFU(implicit p: Parameters) extends FunctionalUnit {
   override def desiredName: String = s"${p(ISA).name}_lsu_fu"
@@ -14,51 +17,50 @@ class LsuFU(implicit p: Parameters) extends FunctionalUnit {
   val mem  = IO(new CacheIO(UInt(p(XLen).W), p(XLen)))
   val mmio = IO(new CacheIO(UInt(p(XLen).W), p(XLen)))
 
-  val lsu       = Module(new Lsu)
+  val core_lsu  = Module(new Lsu)
   val imm_utils = ImmUtilsFactory.getOrThrow(p(ISA).name)
 
-  val uop_reg                                            = Reg(new MicroOp)
-  val state_idle :: state_wait_resp :: state_done :: Nil = Enum(3)
-  val state                                              = RegInit(state_idle)
+  val req_reg = Reg(new MicroOp)
+  val state   = RegInit(LsuFuState.IDLE)
 
-  io.req.ready := (state === state_idle) || (state === state_done && io.resp.fire)
+  io.req.ready := (state === LsuFuState.IDLE) || (state === LsuFuState.DONE && io.resp.fire)
 
   when(io.req.fire) {
-    state   := state_wait_resp
-    uop_reg := io.req.bits
+    state   := LsuFuState.WAIT_RESP
+    req_reg := io.req.bits
   }.elsewhen(io.flush) {
-    state := state_idle
+    state := LsuFuState.IDLE
   }.otherwise {
-    when(state === state_wait_resp) {
-      val resp_fired = lsu.mem.resp.fire || lsu.mmio.resp.fire
+    when(state === LsuFuState.WAIT_RESP) {
+      val resp_fired = core_lsu.mem.resp.fire || core_lsu.mmio.resp.fire
       when(resp_fired) {
-        state := state_done
+        state := LsuFuState.DONE
       }
-    }.elsewhen(state === state_done && io.resp.fire) {
-      state := state_idle
+    }.elsewhen(state === LsuFuState.DONE && io.resp.fire) {
+      state := LsuFuState.IDLE
     }
   }
 
-  val imm  = imm_utils.genImm(uop_reg.instr, uop_reg.imm_type)
-  val addr = uop_reg.rs1_data + imm
+  val imm  = imm_utils.genImm(req_reg.instr, req_reg.imm_type)
+  val addr = req_reg.rs1_data + imm
 
   val (_, pma_readable, pma_writable, pma_cacheable) = PmaChecker(addr)
 
-  lsu.en            := (state === state_wait_resp)
-  lsu.uop           := uop_reg.uop
-  lsu.addr          := addr
-  lsu.wdata         := uop_reg.rs2_data
-  lsu.pma_readable  := pma_readable
-  lsu.pma_writable  := pma_writable
-  lsu.pma_cacheable := pma_cacheable
+  core_lsu.en            := (state === LsuFuState.WAIT_RESP)
+  core_lsu.uop           := req_reg.uop
+  core_lsu.addr          := addr
+  core_lsu.wdata         := req_reg.rs2_data
+  core_lsu.pma_readable  := pma_readable
+  core_lsu.pma_writable  := pma_writable
+  core_lsu.pma_cacheable := pma_cacheable
 
-  mem <> lsu.mem
-  mmio <> lsu.mmio
+  mem <> core_lsu.mem
+  mmio <> core_lsu.mmio
 
-  io.resp.valid        := (state === state_done)
-  io.resp.bits.result  := lsu.rdata
-  io.resp.bits.rd      := uop_reg.rd
-  io.resp.bits.pc      := uop_reg.pc
-  io.resp.bits.instr   := uop_reg.instr
-  io.resp.bits.rob_tag := uop_reg.rob_tag
+  io.resp.valid        := (state === LsuFuState.DONE)
+  io.resp.bits.result  := core_lsu.rdata
+  io.resp.bits.rd      := req_reg.rd
+  io.resp.bits.pc      := req_reg.pc
+  io.resp.bits.instr   := req_reg.instr
+  io.resp.bits.rob_tag := req_reg.rob_tag
 }
--- arch/core/mult/MultFu.scala
+++ arch/core/mult/MultFu.scala
@@ -3,7 +3,10 @@ package arch.core.mult
 import arch.configs._
 import arch.core.ooo._
 import chisel3._
-import chisel3.util._
+
+object MultFuState extends ChiselEnum {
+  val IDLE, BUSY, DONE = Value
+}
 
 class MultFU(implicit p: Parameters) extends FunctionalUnit {
   override def desiredName: String = s"${p(ISA).name}_mult_fu"
@@ -11,41 +14,40 @@ class MultFU(implicit p: Parameters) extends FunctionalUnit {
   val mult       = Module(new Mult)
   val mult_utils = MultUtilsFactory.getOrThrow(p(ISA).name)
 
-  val uop_reg                                       = Reg(new MicroOp)
-  val state_idle :: state_busy :: state_done :: Nil = Enum(3)
-  val state                                         = RegInit(state_idle)
+  val req_reg = Reg(new MicroOp)
+  val state   = RegInit(MultFuState.IDLE)
 
-  io.req.ready := (state === state_idle) || (state === state_done && io.resp.fire)
+  io.req.ready := (state === MultFuState.IDLE) || (state === MultFuState.DONE && io.resp.fire)
 
   when(io.req.fire) {
-    state   := state_busy
-    uop_reg := io.req.bits
+    state   := MultFuState.BUSY
+    req_reg := io.req.bits
   }.elsewhen(io.flush) {
-    state := state_idle
+    state := MultFuState.IDLE
   }.otherwise {
-    when(state === state_busy && mult.done) {
-      state := state_done
-    }.elsewhen(state === state_done && io.resp.fire) {
-      state := state_idle
+    when(state === MultFuState.BUSY && mult.done) {
+      state := MultFuState.DONE
+    }.elsewhen(state === MultFuState.DONE && io.resp.fire) {
+      state := MultFuState.IDLE
     }
   }
 
-  val current_uop = Mux(io.req.fire, io.req.bits.uop, uop_reg.uop)
+  val current_uop = Mux(io.req.fire, io.req.bits.uop, req_reg.uop)
   val ctrl        = mult_utils.decode(current_uop)
 
   mult.en   := io.req.fire
   mult.kill := io.flush
-  mult.src1 := Mux(io.req.fire, io.req.bits.rs1_data, uop_reg.rs1_data)
-  mult.src2 := Mux(io.req.fire, io.req.bits.rs2_data, uop_reg.rs2_data)
+  mult.src1 := Mux(io.req.fire, io.req.bits.rs1_data, req_reg.rs1_data)
+  mult.src2 := Mux(io.req.fire, io.req.bits.rs2_data, req_reg.rs2_data)
 
   mult.a_signed := ctrl.a_signed
   mult.b_signed := ctrl.b_signed
   mult.high     := ctrl.high
 
-  io.resp.valid        := (state === state_done)
+  io.resp.valid        := (state === MultFuState.DONE)
   io.resp.bits.result  := mult.result
-  io.resp.bits.rd      := uop_reg.rd
-  io.resp.bits.pc      := uop_reg.pc
-  io.resp.bits.instr   := uop_reg.instr
-  io.resp.bits.rob_tag := uop_reg.rob_tag
+  io.resp.bits.rd      := req_reg.rd
+  io.resp.bits.pc      := req_reg.pc
+  io.resp.bits.instr   := req_reg.instr
+  io.resp.bits.rob_tag := req_reg.rob_tag
 }
