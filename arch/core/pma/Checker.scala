1ecfcc75043a2505aa8a14b20ecef00b49adafbd
dashygo097 <dashygo097@gmail.com>
2026-04-26 16:46:09 +0800

    [risc] dcache integrated

--- arch/core/pma/Checker.scala
+++ arch/core/pma/Checker.scala
@@ -25,7 +25,7 @@ class PmaChecker(implicit p: Parameters) extends Module {
   valid     := is_sram || is_uart || is_irh
   readable  := is_sram || is_uart || is_irh
   writable  := is_sram || is_uart || is_irh
-  cacheable := false.B
+  cacheable := is_sram
 }
 
 object PmaChecker {
